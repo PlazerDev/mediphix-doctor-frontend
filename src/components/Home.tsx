8a6e15f9fd821f47876c736c7ba2f998287d621e
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-07-30 21:04:21 +0530

    Temperory doctor login done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,20 @@
-import {useAuthContext} from "@asgardeo/auth-react";
-import DoctorHome from "./doctor/DoctorHome.tsx";
+import { useEffect } from 'react';
+import { useAuthContext } from "@asgardeo/auth-react";
+import DoctorHome from "../components/doctor/home/DoctorHome.tsx";
 import GuestHomePage from "./GuestHomePage.tsx";
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-    const {state} = useAuthContext();
-    return (
-        !state.isAuthenticated ?
-            <GuestHomePage/>
-            :
-            <DoctorHome/>
+  const { state } = useAuthContext();
+  const navigate = useNavigate();
 
-    );
+  useEffect(() => {
+    if (state.isAuthenticated) {
+      navigate("/doctor/home");
+    }
+  }, [state.isAuthenticated, navigate]);
+
+  return !state.isAuthenticated ? <GuestHomePage /> : null;
 }
 
-
-export default Home
+export default Home;
