4c8e0ad7c134ada73f43ac8e922aa254e7081bb5
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-07-30 12:03:38 +0530

    Doctor UI replacement

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import {AuthProvider} from "@asgardeo/auth-react";
+import { AuthProvider } from "@asgardeo/auth-react";
 import Home from "./components/Home.tsx";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DoctorRoutes from "./routes/DoctorRoutes.tsx";
 
 function App() {
-    const asgardioConfig = {
-        signInRedirectURL: "http://localhost:5174",
-        signOutRedirectURL: "http://localhost:5174",
-        clientID: import.meta.env.VITE_PATIENT_ASGARDEO_CLIENT_ID,
-        baseUrl: import.meta.env.VITE_PATIENT_ASGARDEO_BASE_URL,
-        scope: ["openid", "email", "profile", "insert_appointment", "retrieve_own_patient_data"]
-    };
-    return (
-        <>
-            <AuthProvider config={asgardioConfig}>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                    </Routes>
-                </Router>
-            </AuthProvider>
-        </>
-    )
+  const asgardioConfig = {
+    signInRedirectURL: "http://localhost:5174",
+    signOutRedirectURL: "http://localhost:5174",
+    clientID: import.meta.env.VITE_PATIENT_ASGARDEO_CLIENT_ID,
+    baseUrl: import.meta.env.VITE_PATIENT_ASGARDEO_BASE_URL,
+    scope: [
+      "openid",
+      "email",
+      "profile",
+      "insert_appointment",
+      "retrieve_own_patient_data",
+    ],
+  };
+  return (
+    <>
+      <AuthProvider config={asgardioConfig}>
+        <Router>
+          <Routes>
+            
+            {/* doctor Rotes  */}
+            <Route path="/doctor/*" element={<DoctorRoutes />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
