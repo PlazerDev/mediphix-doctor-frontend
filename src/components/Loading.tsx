81c067ab3a21129aea24aa227482bb59ea03e206
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-09-04 14:56:52 +0530

    doctor home page ongoingSession -> Service done

---
+++ src/components/Loading.tsx
@@ -0,0 +1,20 @@
+import { PropagateLoader } from "react-spinners";
+
+interface LoadingProps {
+  footer:boolean;
+}
+
+function Loading(props:LoadingProps) {
+  return (
+    <div className="flex-grow p-20 inline-flex flex-col gap-8 justify-center items-center bg-transparent">
+      <PropagateLoader
+        color="#FF7300"
+        className="w-20 items-center justify-center"
+      />
+      { props.footer === true ? <p className="text-mediphix_text_c text-center">Loading, Please Wait</p> : null}
+      
+    </div>
+  );
+}
+
+export default Loading;
